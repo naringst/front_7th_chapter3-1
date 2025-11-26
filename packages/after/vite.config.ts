cb4fc50b06412bcbf2107f5b02190357cb98867c
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-11-27 01:06:16 +0900

    chore: tailwindCSS 설치 및 기본 설정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vite.dev/config/
+const viteConfig = defineConfig({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
-})
+});
+
+export default mergeConfig(
+  viteConfig,
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      css: true,
+    },
+  }),
+);
