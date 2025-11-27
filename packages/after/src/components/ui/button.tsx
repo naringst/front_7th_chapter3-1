33b5ba8e95056f6295f79daadab1dd0c4c9b606e
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-11-28 04:14:19 +0900

    style: button 관련 토큰 생성 및 variants 설정

--- packages/after/src/components/ui/button.tsx
+++ packages/after/src/components/ui/button.tsx
@@ -15,14 +15,14 @@ const buttonVariants = cva(
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+          'bg-secondary text-black border border-[#333333] hover:bg-secondary/80',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
         // 새로 추가
-        primary: 'bg-primary text-primary',
-        success: 'bg-success',
-        danger: 'bg-danger text-danger',
+        primary: 'bg-primary text-white',
+        success: 'bg-success text-white',
+        danger: 'bg-danger text-white',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
