f33d71ce6e9916a6b88df1a8f4f9b281e04f556b
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-11-28 05:53:43 +0900

    feat: gh-pages 배포 설정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vite.dev/config/
 // https://vite.dev/config/
 const viteConfig = defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-1/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
